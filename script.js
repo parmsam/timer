6bcd87ab4fa4b30c5bad7a6598066c8ab6e774d5
Sam <40672605+parmsam@users.noreply.github.com>
2020-10-21 21:53:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     //clearInterval(x);
-    document.getElementById("timer").innerHTML = "TIME IS UP";
     playSound('bump');playSound('bump');playSound('bump');
+    document.getElementById("timer").innerHTML = "TIME IS UP";
     //alert("Your time is up!");
   }}, 1000);
 //every 1000 ms = 1 sec
